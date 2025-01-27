e31721efb1c84ca7e2d419d6609c1bd3b4953198
Rajps55 <hackinggm69@gmail.com>
2025-01-27 23:26:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,6 +42,8 @@ TUT_VID = os.environ.get("TUT_VID","https://t.me/All_Movie_Star_Link")
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002443500050"))
 FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001710724714"))
 JOIN_REQUEST_ENABLE = os.environ.get("JOIN_REQUEST_ENABLED", "True")
+# Second forced subscription channel
+SECOND_JOIN_REQUEST_ENABLE = True  # Second channel ke liye enable/disable toggle
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
